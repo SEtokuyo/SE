dd2badb94952b3d9cb574631babbda13aee4411f
張仲廷 <97320971+nickchang1031@users.noreply.github.com>
2023-06-16 03:05:21 +0800

    models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -19,6 +19,8 @@ class User(AbstractUser):
 
 
 class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     # ...
 
     @classmethod
