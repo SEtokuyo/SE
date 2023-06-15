d927c430af1226915fb29e7548687b95173a1829
張仲廷 <97320971+nickchang1031@users.noreply.github.com>
2023-06-15 23:51:09 +0800

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import ProfileForm, NotificationForm,RegistrationForm
+from django.shortcuts import render
 
 # Create your views here.
 
