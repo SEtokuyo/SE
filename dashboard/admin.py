166dbd223f62abfa43393b51c1fafccd8cefcd77
OdetteHsu <a09664489992144@gmail.com>
2023-06-17 04:54:03 +0800

    db塞塞

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Product, Order, OrderItem, Customer,Role
+from .models import User, Product, Order, OrderItem, Customer, Role, Status,PaymentMethod
 # Register your models here.
 
+
 admin.site.register(User)
+admin.site.register(Status)
+admin.site.register(PaymentMethod)
 admin.site.register(Role)
 admin.site.register(Product)
 admin.site.register(Order)
